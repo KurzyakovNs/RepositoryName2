19757d28dc47dcd39c7279672e6a47bb5f7fc60a
KurzyakovNs <115495749+KurzyakovNs@users.noreply.github.com>
2023-02-05 20:44:33 +0300

    Add files via upload

--- Лабораторная 3/main.py
+++ Лабораторная 3/main.py
@@ -11,21 +11,39 @@ class Book:
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
 
-class PaperBook:
+class PaperBook(Book):
+    """ Дочерний класс книги"""
     def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
-        self.pages = pages
+        super().__init__(name,author)
+        if isinstance(pages, int):
+            if pages > 0:
+                self.pages = pages
+            else:
+                raise AttributeError("error count of pages")
+        else:
+            raise AttributeError("error type of pages")
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Книга {self.name}. Автор {self.author} Страниц {self.pages}"
 
 
-class AudioBook:
+class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
-        self.duration = duration
+        super().__init__(name,author)
+        if isinstance(duration, float):
+            if duration > 0:
+                self.duration = duration
+            else:
+                raise AttributeError(f"error time of duration")
+        else:
+            raise AttributeError(f"error type")
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Книга {self.name}. Автор {self.author}. Длительность {self.duration}"
+
+Book1 = Book('Trainspotting', 'Irvine Welsh')
+print(Book1)
+Book2 = PaperBook('Fight club', 'Chuck Palahniuk', 300)
+print(Book2)
+Book3 = AudioBook('Psycho', 'Robert Bloch', 2.42)
+print(Book3)
